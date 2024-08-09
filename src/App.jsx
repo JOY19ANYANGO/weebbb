4e903e9172b323141c74725179b5de8df54696dd
JOY19ANYANGO <joyanyango402@gmail.com>
2024-08-09 18:00:22 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ function App() {
         <source src={starsVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/uZ55mDL7dA0?si=vz7FshVZCT35Icvu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/EVTXBDuYlKc?si=3FdwFlZJ2kLqIiQe" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       
       {/* Content */}
       <div className='relative z-10 p-10 rounded-lg shadow-lg text-center font-dancing-script'>
-        <h1 className='text-8xl typing mb-4'>Happy Birthday Ezra</h1>
+        <h1 className='text-8xl typing mb-4'>Happy Birthday Hamida</h1>
         <div className='text-5xl mb-4'>🎉🎂🎈</div>
         <p className='text-3xl  mb-2'>Wishing you a fantastic day filled with joy, laughter, and all your favorite things.</p>
         <p className='text-3xl  mb-2'>May this year bring you even more happiness and success. </p>
