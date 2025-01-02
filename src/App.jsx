500f7428b6f1168802b6cdf72bba50ded0649406
JOY19ANYANGO <joyanyango402@gmail.com>
2025-01-02 20:13:22 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,30 @@ function App() {
   return (
     <div className='relative flex flex-col items-center justify-center min-h-screen text-white'>
       <div className='hero-container'>
-      <video autoPlay muted loop id="myVideo" >
-        <source src={starsVideo} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/EVTXBDuYlKc?si=3FdwFlZJ2kLqIiQe" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      
-      {/* Content */}
-      <div className='relative z-10 p-10 rounded-lg shadow-lg text-center font-dancing-script'>
-        <h1 className='text-8xl typing mb-4'>Happy Birthday Hamida</h1>
-        <div className='text-5xl mb-4'>🎉🎂🎈</div>
-        <p className='text-3xl  mb-2'>Wishing you a fantastic day filled with joy, laughter, and all your favorite things.</p>
-        <p className='text-3xl  mb-2'>May this year bring you even more happiness and success. </p>
-        <p className='text-3xl  '>  Cheers to many more amazing moments and memories ahead!</p>
-        <p className='text-3xl typing'>Enjoy your special day!</p>
-        
-      </div>
+        <video autoPlay muted loop id="myVideo">
+          <source src={starsVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        {/* Content */}
+        <div className='relative z-10 p-10 rounded-lg shadow-lg text-center font-dancing-script'>
+          <h1 className='text-8xl typing mb-4'>Happy New Year, Tim! 🎆</h1>
+          <div className='text-5xl mb-4'>🎉💖✨</div>
+          <p className='text-3xl mb-2'>
+            Thank you for being one of my closest friends in 2024. Your support and presence meant the world to me.
+          </p>
+          <p className='text-3xl mb-2'>
+            Wishing you an amazing 2025 filled with happiness, success, and unforgettable memories. 
+          </p>
+          <p className='text-3xl'>Cheers to a year full of love and laughter! ❤️🥂</p>
+          <p className='text-3xl typing'>You’re truly appreciated. Happy New Year! 💕</p>
+        </div>
       </div>
     </div>
   );
 }
 
 export default App;
+
 
 
